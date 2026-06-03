ba948677eebb54da20b0762a11ca963acaca5ef1
Owen O'Connor <tux2603@gmail.com>
2026-06-02 23:55:52 -0500

    Add buttons

---
+++ hdl/peripherals/buttons.v
@@ -0,0 +1,124 @@
+`timescale 1ns / 1ps
+
+
+module buttons #(
+        parameter C_S_AXI_DATA_WIDTH = 32,
+        parameter C_S_AXI_ADDR_WIDTH = 3, // 8 buttons, 1 byte each
+        parameter C_S_AXIS_DATA_WIDTH = 16
+    ) (
+        // AXI4-Lite Slave Interface
+        input wire s_axi_aclk,
+        input wire s_axi_aresetn,
+        input wire [C_S_AXI_ADDR_WIDTH-1:0] s_axi_awaddr,
+        input wire [2:0] s_axi_awprot,
+        input wire s_axi_awvalid,
+        output reg s_axi_awready = 1'b1,
+        input wire [C_S_AXI_DATA_WIDTH-1:0] s_axi_wdata,
+        input wire [C_S_AXI_DATA_WIDTH/8-1:0] s_axi_wstrb,
+        input wire s_axi_wvalid,
+        output reg s_axi_wready = 1'b1,
+        output wire [1:0] s_axi_bresp,
+        output reg s_axi_bvalid = 1'b0,
+        input wire s_axi_bready,
+        input wire [C_S_AXI_ADDR_WIDTH-1:0] s_axi_araddr,
+        input wire [2:0] s_axi_arprot,
+        input wire s_axi_arvalid,
+        output reg s_axi_arready = 1'b1,
+        output reg [C_S_AXI_DATA_WIDTH-1:0] s_axi_rdata,
+        output wire [1:0] s_axi_rresp,
+        output reg s_axi_rvalid = 1'b0,
+        input wire s_axi_rready,
+
+        // AXI4 Stream slave interface
+        input wire s_axis_aclk,
+        input wire s_axis_aresetn,
+        input wire [C_S_AXIS_DATA_WIDTH-1:0] s_axis_tdata,
+        output reg s_axis_tready = 1'b1,
+        input wire s_axis_tvalid,
+
+        // GPIO inputs
+        input wire [3:0] buttons_in
+    );
+
+    localparam NUM_REGS = (2**C_S_AXI_ADDR_WIDTH) / (C_S_AXI_DATA_WIDTH / 8);
+    localparam ADDR_LSB = (C_S_AXI_DATA_WIDTH/32) + 1;
+    localparam MEM_ADDR_BITS = C_S_AXI_ADDR_WIDTH - ADDR_LSB;
+
+    // AXI4-Lite registers
+    reg [C_S_AXI_DATA_WIDTH-1:0] button_values[0:NUM_REGS-1];
+
+
+    // For now, just take the value from the ADC and expose it on the AXI4-Lite interface at address 0
+    // read in all four external buttons. expose these on the AXI4-Liter interface at address 4, 5, 6, and 7
+
+    // Do some basic CDC on the button inputs to avoid metastability issues
+    reg [3:0] buttons_cdc0;
+    reg [3:0] buttons_cdc1;
+
+    always @(posedge s_axi_aclk) begin
+        if (!s_axi_aresetn) begin
+            buttons_cdc0 <= 4'b0;
+            buttons_cdc1 <= 4'b0;
+        end else begin
+            buttons_cdc0 <= buttons_in;
+            buttons_cdc1 <= buttons_cdc0;
+        end
+    end
+
+    // Update the button values in the AXI4-Lite registers
+    always @(posedge s_axi_aclk) begin
+        if (!s_axi_aresetn) begin
+            button_values[1] <= 32'b0;
+        end else begin
+            button_values[1] <= {
+                buttons_cdc1[3] ? 8'h01 : 8'h00,
+                buttons_cdc1[2] ? 8'h01 : 8'h00,
+                buttons_cdc1[1] ? 8'h01 : 8'h00,
+                buttons_cdc1[0] ? 8'h01 : 8'h00
+            };
+        end;
+    end
+
+    // AXI4 Stream coming into the module, just expose the value on the AXI4-Lite interface at address 0
+    always @(posedge s_axis_aclk) begin
+        if (!s_axis_aresetn) begin
+            button_values[0] <= 32'b0;
+        end else if (s_axis_tvalid) begin
+            button_values[0] <= {16'b0, s_axis_tdata};
+        end
+    end
+
+    // We don't need AXI4-Lite write functionality for this module, so just return an error if the master tries to write to any address
+    always @(posedge s_axi_aclk) begin
+        if (!s_axi_aresetn) begin
+            s_axi_bvalid <= 1'b0;
+        end else if (s_axi_awvalid && s_axi_wvalid) begin
+            s_axi_bvalid <= 1'b1;
+        end else if (s_axi_bvalid && s_axi_bready) begin
+            s_axi_bvalid <= 1'b0;
+        end
+    end
+
+    assign s_axi_bresp = 2'b11; // SLVERR
+
+    // Handle AXI4-Lite read transactions
+    always @(posedge s_axi_aclk) begin
+        if (!s_axi_aresetn) begin
+            s_axi_rvalid <= 1'b0;
+            s_axi_rdata <= 'b0;
+            s_axi_arready <= 1'b1;
+        end else begin
+            if (s_axi_arvalid && s_axi_arready) begin
+                s_axi_rdata <= button_values[s_axi_araddr[ADDR_LSB +: MEM_ADDR_BITS]];
+                s_axi_rvalid <= 1'b1;
+                s_axi_arready <= 1'b0;
+            end else if (s_axi_rvalid && s_axi_rready) begin
+                s_axi_rvalid <= 1'b0;
+                s_axi_arready <= 1'b1;
+            end
+        end
+    end
+
+    // Since we only support read transactions, we can just return OKAY for all reads
+    assign s_axi_rresp = 2'b00; // OKAY
+endmodule
--- vivado_project/orbit_fpga.srcs/constrs_1/new/pin_mappings.xdc
+++ vivado_project/orbit_fpga.srcs/constrs_1/new/pin_mappings.xdc
@@ -4,3 +4,11 @@ set_property -dict {PACKAGE_PIN R16 IOSTANDARD LVCMOS33} [get_ports {lcd_data[2]
 set_property -dict {PACKAGE_PIN U17 IOSTANDARD LVCMOS33} [get_ports {lcd_data[3]}]
 set_property -dict {PACKAGE_PIN V17 IOSTANDARD LVCMOS33} [get_ports {lcd_rs}]
 set_property -dict {PACKAGE_PIN V18 IOSTANDARD LVCMOS33} [get_ports {lcd_en}]
+
+set_property -dict {PACKAGE_PIN D19 IOSTANDARD LVCMOS33} [get_ports {buttons_in[0]}]
+set_property -dict {PACKAGE_PIN D20 IOSTANDARD LVCMOS33} [get_ports {buttons_in[1]}]
+set_property -dict {PACKAGE_PIN L20 IOSTANDARD LVCMOS33} [get_ports {buttons_in[2]}]
+set_property -dict {PACKAGE_PIN L19 IOSTANDARD LVCMOS33} [get_ports {buttons_in[3]}]
+
+set_property -dict {PACKAGE_PIN E17 IOSTANDARD LVCMOS33} [get_ports {Vp_Vn_vp_in}]
+set_property -dict {PACKAGE_PIN D18 IOSTANDARD LVCMOS33} [get_ports {Vp_Vn_vn_in}]
